0fd71978a29995631372d0b7e3eb930a734cd0ab
Rikky Jakhar <rikkyj108@gmail.com>
2026-09-24 12:22:33 +0530

    leetcode: add maximum-points-you-can-obtain-from-cards (python3)

--- leetcode/maximum-points-you-can-obtain-from-cards/solution.py
+++ leetcode/maximum-points-you-can-obtain-from-cards/solution.py
@@ -1,1 +1,25 @@
-c
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int ans = Integer.MIN_VALUE;
+        int sum = 0;
+        int n = cardPoints.length;
+        for(int i : cardPoints){
+            sum += i;
+        }
+        int left = 0;
+
+        for(int right = 0; right <n && right < k; right++){
+            int start = 0;
+            for(int i = 0; i<right; i++){
+                start += cardPoints[i];
+            }
+            int end = 0;
+            int x = n-k+right;
+            for(int j =x; j <n ; j++ ){
+                end += cardPoints[j];
+            }
+            ans = Math.max(ans, start + end);
+        }
+        return ans;
+    }
+}
